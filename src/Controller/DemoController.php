4fc720165f86c677f3ccadd437156da6ee78818d
thetimverse <tim.mockingjay09@gmail.com>
2023-10-02 10:12:13 +0200

    fix: merge problem

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\SlugService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class DemoController extends AbstractController
 {
     #[Route('/demo', name: 'app_demo')]
-    public function index(): Response
+    public function index(SlugService $slugService): Response
     {
+        $result = $slugService -> makeSlug('Wôrķšƥáçè ~~sèťtïñğš~~');
+
         return $this->render('demo/index.html.twig', [
             'controller_name' => 'DemoController',
+            'result' => $result,
         ]);
     }
 }
